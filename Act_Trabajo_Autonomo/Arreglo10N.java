8621f0747cb62b4b4df2fcf8c0b376536e728751
AlanVasquezAriza <124604196+AlanVasquezAriza@users.noreply.github.com>
2023-08-11 15:14:29 -0500

    Update Arreglo10N.java
    Update clase

--- Act_Trabajo_Autonomo/Arreglo10N.java
+++ Act_Trabajo_Autonomo/Arreglo10N.java
@@ -1,23 +1,9 @@
 package guia.pkg01;
-public class Guia01 {
+public class Arreglo10N {
     public static void main(String[] args) {
         int[] numeros = {1, 2, 3, 4, 5 ,6 ,7 ,8 ,9 ,10};
         for (int i = numeros.length - 1; i >= 0; i--) {
             System.out.print(numeros[i]+ ", ");
         }
     }
-    public abstract class ClaseAbstracta {
-    private int atributo1;
-    protected String atributo2;
-    public double atributo3;
-
-    public ClaseAbstracta(int valor1, String valor2, double valor3) {
-        this.atributo1 = valor1;
-        this.atributo2 = valor2;
-        this.atributo3 = valor3;
-    }
-
-    public abstract void metodoAbstracto();
-}
-
 }
