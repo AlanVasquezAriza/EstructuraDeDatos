59c839e65847403fbe6864caef96e0ee59d5cedc
SebastianGit110 <124834599+SebastianGit110@users.noreply.github.com>
2023-09-13 11:36:27 -0500

    Nuevos metodos
    Metodos que muestran informacion sobre los empleados

--- HashSet/src/HashTableProject/Empleado.java
+++ HashSet/src/HashTableProject/Empleado.java
@@ -3,6 +3,7 @@ package HashTableProject;
 import java.util.Hashtable;
 import java.util.Map;
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class Empleado {
     
@@ -16,7 +17,7 @@ public class Empleado {
     
     protected int sueldoEmpleado = 0;
     protected double sueldoMasCinco = 0, totalBonificaciones = 0;
-    protected int array [] = new int [10];
+    protected int array [] = new int [100];
     
     //Constructor
     public Empleado(){}
@@ -27,7 +28,7 @@ public class Empleado {
     }
     
     public void llenarSueldo(int pos){
-        sueldo.put(pos, sueldoEmpleado = random.nextInt(2001) + 1000);
+        sueldo.put(pos, sueldoEmpleado = random.nextInt(2001) + 100);
     }
     
     //Muestra la informacion de los empleados
@@ -39,33 +40,33 @@ public class Empleado {
     
     //Se llena un array con los los sueldos
     public void comparaPrimerosTreinta(){
-        for(int i=0;i<=9;i++){
+        for(int i=0;i<=99;i++){
             array[i] = sueldo.get(i);
         }
-        mostrarArray();
+        //mostrarArray();
         OrganizarSueldos();
-        mostrarArray();
+        //mostrarArray();
         System.out.println();
         
-        for(int i=1;i<=9;i++){
-            for(int j=0;j<=9;j++){
-                
-                if(i == 1){
-                    if(array[0] == sueldo.get(j)){
-                        System.out.println("**El valor: " + array[0] + " esta en la posicion " + (j));
-                    }
-                }
-                
-                if(array[i] == array[i-1]){
-                    break;
-                }
-                
-                if(array[i] == sueldo.get(j)){
-                    System.out.println("El valor: " + array[i] + " esta en la posicion " + j);
-                }
-                
-            }
-        }
+//        for(int i=1;i<=99;i++){
+//            for(int j=0;j<=99;j++){
+//                
+//                if(i == 1){
+//                    if(array[0] == sueldo.get(j)){
+//                        System.out.println("**El valor: " + array[0] + " esta en la posicion " + (j));
+//                    }
+//                }
+//                
+//                if(array[i] == array[i-1]){
+//                    break;
+//                }
+//                
+//                if(array[i] == sueldo.get(j)){
+//                    System.out.println("El valor: " + array[i] + " esta en la posicion " + j);
+//                }
+//                
+//            }
+//        }
     }
     
     public void bonificacion5Porciento(){
@@ -73,7 +74,7 @@ public class Empleado {
         System.out.println("Los empleados identificados con los siguientes RUT han sido beneficiados con aumento del 5% de su sueldo: ");
         System.out.println("    RUT    ||    Sueldo    ||      +5%    ");
         
-        for(int i=0;i<=4;i++){ //Se limita el for para dar la bonificacion
+        for(int i=0;i<=29;i++){ //Se limita el for para dar la bonificacion
             if(sueldo.containsValue(array[i])){
             
                 for (Map.Entry<Integer, Integer> entry : sueldo.entrySet()) {
@@ -88,7 +89,42 @@ public class Empleado {
             }
         }
         System.out.println("Costo total de las bonificaciones: " + totalBonificaciones);
-        
+    }
+    
+    public void menorBonificacion(){
+        System.out.println("\nEmpleado con menor bonificacion: ");
+        Integer claveCorrespondiente = 0;
+        for (Map.Entry<Integer, Integer> entry : sueldo.entrySet()) {
+            if (entry.getValue().equals(array[0])) {
+                claveCorrespondiente = entry.getKey();
+                break; 
+            }
+        }
+        System.out.println("Rut " + rut.get(claveCorrespondiente) + " con sueldo de: " + sueldo.get(claveCorrespondiente));
+    }
+    
+    public void mayorBonificacion(){
+        System.out.println("\nEmpleado con mayor bonificacion: ");
+        Integer claveCorrespondiente = 0;
+        for (Map.Entry<Integer, Integer> entry : sueldo.entrySet()) {
+            if (entry.getValue().equals(array[29])) {
+                claveCorrespondiente = entry.getKey();
+                break; 
+            }
+        }
+        System.out.println("Rut " + rut.get(claveCorrespondiente) + " con sueldo de: " + sueldo.get(claveCorrespondiente));
+    }
+    
+    public void encontrarEmpleadoRut(int rutBuscar){
+        Integer claveCorrespondiente = 0;
+        for (Map.Entry<Integer, Integer> entry : rut.entrySet()) {
+            if (entry.getValue().equals(rutBuscar)) {
+                claveCorrespondiente = entry.getKey();
+                break; 
+            }
+        }
+        System.out.println("\nRut ingresado: " + rutBuscar);
+        System.out.println("Empleado " + claveCorrespondiente + " con rut " + rut.get(claveCorrespondiente) + " sueldo: " + sueldo.get(claveCorrespondiente));
     }
     
     public void OrganizarSueldos(){
